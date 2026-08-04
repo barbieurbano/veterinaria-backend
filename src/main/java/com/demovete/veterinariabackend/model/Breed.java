c05aa9a26f8962dd8d62080cd639d3f2863fb239
Barbara Urbano <sbaarby@gmail.com>
2026-08-04 20:55:13 +0200

    #7 Se crea la entidad breed con sus atributos, SIN asociaciones

---
+++ src/main/java/com/demovete/veterinariabackend/model/Breed.java
@@ -0,0 +1,21 @@
+package com.demovete.veterinariabackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "breeds")
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Breed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+}
