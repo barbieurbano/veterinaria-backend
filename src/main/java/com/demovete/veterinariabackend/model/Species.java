160835f144f6359820514663ed480558595362e9
Barbara Urbano <sbaarby@gmail.com>
2026-08-04 21:00:29 +0200

    #8 Se crea la entidad specie con sus atributos

---
+++ src/main/java/com/demovete/veterinariabackend/model/Species.java
@@ -0,0 +1,21 @@
+package com.demovete.veterinariabackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "species")
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Species {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+}
