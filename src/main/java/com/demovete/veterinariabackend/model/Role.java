25099a860151e50009038a91152e01aedcf5af74
Barbara Urbano <sbaarby@gmail.com>
2026-08-05 11:11:56 +0200

    #16 Se crea la entidad Role con sus atributos y tenemos un set con permission porque en un Rol no existen permisos duplicados, un rol puede tener muchos permisos, usamos Fetch.EAGER porque al cargar usuario y rol en memoria que cargue todos sus permisos. En BD crear la tabla intermedia.

---
+++ src/main/java/com/demovete/veterinariabackend/model/Role.java
@@ -0,0 +1,25 @@
+package com.demovete.veterinariabackend.model;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "role_permissions",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions = new HashSet<>();
+}
