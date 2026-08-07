134546650c0c1e4d4883276a36653587a9a0901e
Barbara Urbano <sbaarby@gmail.com>
2026-08-07 16:35:10 +0200

    #21 Se crea la entidad empleado con sus atributos y relaciones con otras entidades

---
+++ src/main/java/com/demovete/veterinariabackend/model/Employee.java
@@ -0,0 +1,48 @@
+package com.demovete.veterinariabackend.model;
+
+import com.demovete.veterinariabackend.model.enums.ContractType;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+
+
+    private String email;
+
+    @Column(nullable = false, length = 30)
+    private Integer phone;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    private Boolean isActive = true;
+
+    @Enumerated(EnumType.STRING)
+    private ContractType contractType;
+
+    private BigDecimal salary;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", unique = true, nullable = true)
+    private User user;
+
+    @OneToOne(mappedBy = "employee", fetch = FetchType.LAZY)
+    private Veterinarian veterinarianProfile;
+}
+
