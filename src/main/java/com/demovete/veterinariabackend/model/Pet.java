6009bbca26fbf5dc101a2be1dc69472f07bc08ce
Barbara Urbano <sbaarby@gmail.com>
2026-08-07 15:42:30 +0200

    #3 Entity Pet (antes animal) con nuevos atributos y relacion con breed y customer.

---
+++ src/main/java/com/demovete/veterinariabackend/model/Pet.java
@@ -0,0 +1,64 @@
+package com.demovete.veterinariabackend.model;
+import com.demovete.veterinariabackend.model.enums.Gender;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "pets")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Pet {
+    //1. ATRIBUTOS PROPIOS
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 60)
+    private String name;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "microchip_number", unique = true, length = 50)
+    private String microchipNumber;
+
+    private Boolean neutered;
+
+    @Column(name = "neutering_date")
+    private LocalDate neuteringDate;
+
+    @Column(length = 50)
+    private String color;
+
+    @Column(name = "distinctive_marks", length = 255)
+    private String distinctiveMarks;
+
+    private String photoUrl;
+
+
+    @Column(columnDefinition = "BOOLEAN DEFAULT true")
+    private Boolean active = true;
+
+    @Column(columnDefinition = "TEXT")
+    private String notes;
+
+    //ATRIBUTOS POR REFERENCIA
+    @ManyToOne
+    @JoinColumn(name = "breed_id", nullable = false)
+    private Breed breed;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+
+}
--- src/main/java/com/demovete/veterinariabackend/model/User.java
+++ src/main/java/com/demovete/veterinariabackend/model/User.java
@@ -16,10 +16,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String firstName;
-    private String lastName;
-    private String phone;
     private String email;
+
+
     private String password;
     private Boolean isRegistered = false;
 
