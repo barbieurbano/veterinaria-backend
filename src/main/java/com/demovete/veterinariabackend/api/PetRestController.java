a8b57f218bf41935efabc909f3e73fb58415d3e2
Barbara Urbano <sbaarby@gmail.com>
2026-08-14 20:13:45 +0200

    #6 se quita el cross

--- src/main/java/com/demovete/veterinariabackend/api/PetRestController.java
+++ src/main/java/com/demovete/veterinariabackend/api/PetRestController.java
@@ -8,7 +8,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/pets")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
 public class PetRestController {
     private final PetService petService;
 
