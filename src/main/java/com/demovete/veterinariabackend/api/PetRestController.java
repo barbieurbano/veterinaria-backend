9bf3adc7a24821fabc4417409370ee23cc2dba0e
Barbara Urbano <sbaarby@gmail.com>
2026-08-14 19:57:36 +0200

    #6 api rest POST pets

---
+++ src/main/java/com/demovete/veterinariabackend/api/PetRestController.java
@@ -0,0 +1,28 @@
+package com.demovete.veterinariabackend.api;
+
+import com.demovete.veterinariabackend.dto.PetCreateRequestDTO;
+import com.demovete.veterinariabackend.dto.PetResponseDTO;
+import com.demovete.veterinariabackend.service.PetService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/pets")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class PetRestController {
+    private final PetService petService;
+
+    @PostMapping
+    public ResponseEntity<PetResponseDTO> createPet(@Valid @RequestBody PetCreateRequestDTO dto){
+        PetResponseDTO createdPet = petService.createPet(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPet);
+    }
+}
