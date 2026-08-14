5cd721030e6f5c0c5584104a580c6a0a9d1761bd
Barbara Urbano <sbaarby@gmail.com>
2026-08-14 12:16:10 +0200

    #6 Se crea PetRepository con consultas personalizadas.

---
+++ src/main/java/com/demovete/veterinariabackend/repository/PetRepository.java
@@ -0,0 +1,21 @@
+package com.demovete.veterinariabackend.repository;
+
+import com.demovete.veterinariabackend.model.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+//Esta anotacion es opcional
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Long> {
+    //Para buscar las mascotas activas de un cliente
+    List<Pet> findByCustomerIdAndActiveTrue(Long customerId);
+
+    //Para validar duplicados buscar por microchip
+    Optional<Pet> findByMicrochipNumber(String microchipNumber);
+
+    //Verifica si existe un microchip asignado a otra mascota.
+    boolean existsByMicrochipNumber(String microchipNumber);
+}
